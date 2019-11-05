bee2ba71d173cb77c478b009995e28f04cd980f5
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-05 17:55:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,7 +11,7 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
-def speak_to_grandma("string")
+def speak_to_grandma(string)
         if "string".upcase
         puts "NO, NOT SINCE 1938!"
         elsif "string".downcase
