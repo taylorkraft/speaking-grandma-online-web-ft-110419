19184366eba3a0384cd819ecfdbe89b6e29f812b
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-05 18:35:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma(string)
         if string.upcase
           return "NO NOT SINCE 1938!"
-          if string == "I LOVE YOU GRANDMA!"
+          elsif string == "I LOVE YOU GRANDMA!"
           return "I LOVE YOU PUMPKIN!"
         else
         return "HUH?! SPEAK UP, SONNY!"
